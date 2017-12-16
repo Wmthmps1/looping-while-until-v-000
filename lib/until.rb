4707814768051b4e448fc74c869025e4edd39868
William Thompson <wmthmps1@gmail.com>
2017-12-16 18:10:43 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,6 @@
 def using_until
 
         levitation_force = 6
-    until levitation_force == 10
-        prints "Wingardium Leviosa/n"
-        levitation_force = levitation_force + 1
-   
+    prints"work"
+    
+end
