854d0e86fd3b17a00c2db95fdb79beaf84776984
William Thompson <wmthmps1@gmail.com>
2017-12-16 18:01:48 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,9 @@
-def using_while levitation_force !=10
+def using_while
 	levitation_force = 6
-  levitation_force = levitation_force +=
+
+  while levitation_force !=10
+		 
+		levitation_force = levitation_force +=
+	end
 
 end
